7ab8581162f05e803711c4c6b9665ed74d8b1794
Mohsin illahi <mbhutto424@gmail.com>
2025-09-13 13:00:10 +0500

    new dev - added armstrong pattern in the series of java basic to advance

--- basic core java/controlstatements/Armstrong.java
+++ basic core java/controlstatements/Armstrong.java
@@ -1,5 +1,35 @@
 package controlstatements;
 
 public class Armstrong {
+    public static void main(String[] args) {
+        armstrongNumber(153);
+    }
+
+    static void armstrongNumber(int no) {
+        int originalNumber = no;
+        int temp = originalNumber;
+        int power = 0;
+        //find power
+        while (temp != 0) {
+            temp = temp / 10;
+            power++;
+        }
+        //to perform sum 
+        int temp2 = originalNumber;
+        int sum = 0;
+        while (temp2 != 0) {
+            int lastDigit = temp2 % 10;
+            sum = (int) (sum + Math.pow(lastDigit, power));
+            temp2 = temp2 / 10;
+        }
+        //condition to check
+        if (originalNumber == sum) {
+
+            System.out.println(originalNumber + " Armstrong number");
+        } else {
+            System.out.println(originalNumber + " Not Armstrong number");
+
+        }
+    }
 
 }
