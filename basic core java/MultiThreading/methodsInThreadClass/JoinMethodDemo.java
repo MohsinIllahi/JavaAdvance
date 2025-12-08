da9a241c016b1aace410602e318d1b50db701796
Mohsin illahi <mbhutto424@gmail.com>
2025-12-08 23:49:28 +0500

    new dev - added example of join method of Thread class

---
+++ basic core java/MultiThreading/methodsInThreadClass/JoinMethodDemo.java
@@ -0,0 +1,41 @@
+package MultiThreading.methodsInThreadClass;
+
+public class JoinMethodDemo
+{
+    public static void main(String[] args) throws InterruptedException
+    {
+        myThread t = new myThread();
+        t.start();
+        // main thread will wait until the t Thread will complete its task then
+        // main thred wil get chance of execution.
+        // therad contains 3 overloaded methods for join() method and it is
+        // important to handle the exception called InterruptedException because
+        // it is checked exception.
+        // Windows dont support yield and join as practically checked by me at
+        // this time.
+        t.join();
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("Main Thread");
+        }
+    }
+}
+
+class myThread extends Thread
+{
+    public void run()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("Child Thread");
+            try
+            {
+                Thread.sleep(20000);
+            } catch (Exception e)
+            {
+                // TODO: handle exception
+            }
+        }
+
+    }
+}
