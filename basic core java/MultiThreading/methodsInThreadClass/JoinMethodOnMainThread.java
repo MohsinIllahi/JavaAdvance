9a84e16b5a263e4fce1dba1a96a8fccc3a42244d
Mohsin illahi <mbhutto424@gmail.com>
2025-12-09 23:58:32 +0500

    refactor - formatted file

--- basic core java/MultiThreading/methodsInThreadClass/JoinMethodOnMainThread.java
+++ basic core java/MultiThreading/methodsInThreadClass/JoinMethodOnMainThread.java
@@ -1,31 +1,37 @@
 package MultiThreading.methodsInThreadClass;
 
-public class JoinMethodOnMainThread {
-public static void main(String[] args) {
-    myThread.mt= Thread.currentThread();
-    myThread t = new myThread();
-    t.start();
-    for (int i = 0; i < 10; i++) {
-        System.out.println("Main Thraed");
+public class JoinMethodOnMainThread
+{
+    public static void main(String[] args)
+    {
+        myThread.mt = Thread.currentThread();
+        myThread t = new myThread();
+        t.start();
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("Main Thraed");
+        }
     }
 }
-}
+
 class myThread extends Thread
 {
     static Thread mt;
-    
-    public void run(){
-    try{
-        mt.join();
 
-    
-      for (int i = 0; i < 10; i++) {
-        Thread.sleep(2000);
-        System.out.println("Child Thraed");
+    public void run()
+    {
+        try
+        {
+            mt.join();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.sleep(2000);
+                System.out.println("Child Thraed");
+            }
+        } catch (InterruptedException e)
+        {
+
+        }
     }
-    }catch (InterruptedException e){
-
-    }   
-    }    
-    }
-
+}
